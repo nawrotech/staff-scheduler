89126ed62ef2622777172ca204f9895291448276
nawrotech <nawrot13@protonmail.com>
2025-04-30 17:11:31 +0200

    add status update for the assignment in AssignmentController

--- src/Controller/Admin/AssignmentController.php
+++ src/Controller/Admin/AssignmentController.php
@@ -3,9 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Assignment;
+use App\Enum\AssignmentStatus;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\ExpressionLanguage\Expression;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsCsrfTokenValid;
@@ -20,6 +22,37 @@ final class AssignmentController extends AbstractController
         ]);
     }
 
+    #[Route('/assignments/update-status/{id}', name: 'admin_assignment_update_status', methods: ['POST'])]
+    #[IsCsrfTokenValid(new Expression('"update-assignment-" ~ args["assignment"].getId()'), tokenKey: 'token')]
+    public function update(
+        Assignment $assignment,
+        EntityManagerInterface $em,
+        Request $request
+    ): Response {
+
+        $submittedStatusValue = $request->getPayload()->get('status');
+
+        if (null === $submittedStatusValue) {
+            $this->addFlash('error', 'Status value not provided.');
+            return $this->redirectToRoute('admin_shift_manage', ['id' => $assignment->getShift()?->getId()]);
+        }
+
+        $newStatus = AssignmentStatus::tryFrom($submittedStatusValue);
+
+        if (null === $newStatus) {
+            $this->addFlash('error', sprintf('Invalid status value "%s" provided.', $submittedStatusValue));
+            return $this->redirectToRoute('admin_shift_manage', ['id' => $assignment->getShift()?->getId()]);
+        }
+        $assignment->setStatus($newStatus);
+        $em->flush();
+
+        $this->addFlash('success', 'Assignment status updated successfully.');
+
+        return $this->redirectToRoute('admin_shift_manage', [
+            'id' => $assignment->getShift()->getId()
+        ]);
+    }
+
     #[Route('/assignment/{id}', name: 'admin_assignment_delete', methods: ['DELETE'])]
     #[IsCsrfTokenValid(new Expression('"delete-assignment-" ~ args["assignment"].getId()'), tokenKey: 'token')]
     public function delete(
@@ -27,15 +60,13 @@ final class AssignmentController extends AbstractController
         EntityManagerInterface $em,
     ): Response {
 
-        $shiftId = $assignment->getShift()->getId();
-
         $em->remove($assignment);
         $em->flush();
 
         $this->addFlash('success', 'Assignment removed successfully.');
 
         return $this->redirectToRoute('admin_shift_manage', [
-            'id' => $shiftId
+            'id' => $assignment->getShift()->getId()
         ]);
     }
 }
